8bbfcd09afcfd45204fb73679b5395b3e68053d3
uenobu106 <uenobu106@gmail.com>
2021-08-08 18:38:00 +0900

    hello rust

---
+++ hello_cargo/target/debug/deps/hello_cargo-0ed49cb6e5fd88e6.d
@@ -0,0 +1,5 @@
+C:\Users\ueno.nobuhiro\workspace\rust-train\hello\hello_cargo\target\debug\deps\hello_cargo-0ed49cb6e5fd88e6.rmeta: src\main.rs
+
+C:\Users\ueno.nobuhiro\workspace\rust-train\hello\hello_cargo\target\debug\deps\hello_cargo-0ed49cb6e5fd88e6.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\ueno.nobuhiro\workspace\rust-train\hello\hello_cargo\target\debug\hello_cargo.exe: C:\Users\ueno.nobuhiro\workspace\rust-train\hello\hello_cargo\src\main.rs
