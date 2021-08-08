a3b2325ac388e0ed4e02db2ad3029a98de63daa4
uenobu106 <uenobu106@gmail.com>
2021-08-08 19:32:57 +0900

    guessing game add

--- README.md
+++ README.md
@@ -6,3 +6,36 @@
 
 # リリースビルド
 - `cargo build --release`
+
+
+# 数当てゲームをプログラムする
+- `/guessing_game`
+- `println!`は、文字列を画面に表示するマクロ
+- `std::io`ライブラリを使用することで、ユーザ入力を受け付ける能力などの実用的な機能の多くを使用することができる
+- `&`という記号は、この引数が参照であることを表す
+- `io::Result`オブジェクト
+  - expectメソッド
+    - Err値の場合、expectメソッドはプログラムをクラッシュさせ、 引数として渡されたメッセージを表示
+```
+let foo = 5; // immutable
+let mut bar = 5; // mutable
+```
+
+## クレートを使用して機能を追加
+```Cargo.toml
+[dependencies]
+rand = "0.4.0"
+```
+- match式
+- cmpメソッドは2値を比較
+- .trim().parse()
+
+## ループで複数回の予想を可能にする
+- roop
+## 正しい予想をした後に終了する
+- break
+## 不正な入力を処理する
+- Result型
+  - OK
+  - Err
+- continue
---
+++ guessing_game/src/main.rs
@@ -0,0 +1,38 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+
+fn main() {
+    println!("Guess the number!");          // 数を当ててごらん
+    
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    //println!("The secret number is: {}", secret_number);    //秘密の数字は次の通り: {}
+
+    println!("Please input your guess.");   // ほら、予想を入力してね
+
+    loop {
+        let mut guess = String::new(); //可変変数 Stringオブジェクト
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");     // 行の読み込みに失敗しました
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        
+        println!("You guessed: {}", guess);     // 次のように予想しました: {}
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),       //小さすぎ！
+            Ordering::Greater => println!("Too big!"),      //大きすぎ！
+            Ordering::Equal => {
+                println!("You win!");        //やったね！
+                break;
+            }
+        }
+    }
+}
